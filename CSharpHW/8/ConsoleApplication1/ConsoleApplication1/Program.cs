0dbc4e391e15d35599ca085ce1cd5eedd081b957
Fedorov Sergii <crustydota@gmail.com>
2016-06-05 14:38:41 +0300

    Task 8

---
+++ CSharpHW/8/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        struct User1
+        {
+            public int age;
+            public string name;
+            public User1(int age, string name)
+            {
+                this.age = age;
+                this.name = name;
+            }
+            public void show()
+            {
+                Console.WriteLine("-----------------");
+                Console.WriteLine("Name: "+ name);
+                Console.WriteLine("Age" + age);
+                Console.WriteLine("-----------------");
+            }
+        }
+        class User
+        {
+            public int age;
+            public string name;
+            public User(int age, string name)
+            {
+                this.age = age;
+                this.name = name;
+            }
+            public void show()
+            {
+                Console.WriteLine("-----------------");
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Age" + age);
+                Console.WriteLine("-----------------");
+            }
+        }
+        static void changeUser(User1 user)
+        {
+            user.name = "Peter";
+            user.age = 42;
+        }
+        static void changeUser(User user)
+        {
+            user.name = "Andrew";
+            user.age = 45;
+        }
+
+        static void Main(string[] args)
+        {
+            User1 u1 = new User1(20, "Vova");
+            u1.show();
+            changeUser(u1);
+            u1.show(); //nothing was changed in u1
+            Console.WriteLine("------------------------------------------------");
+            User u2 = new User(21, "Petya");
+            u2.show();
+            changeUser(u2);
+            u2.show();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpHW/8/ConsoleApplication1/ConsoleApplication2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        enum CarColor { blue, yellow, red, green}
+        enum CarTransmission { auto, manual}
+        class Engine
+        {
+            public int power;
+            public Engine(int power)
+            {
+                this.power = power;
+            }
+        }
+        class Color
+        {
+            public CarColor color;
+            public Color(CarColor color)
+            {
+                this.color = color;
+            }
+        }
+        class Transmission
+        {
+            public CarTransmission transmission;
+            public Transmission(CarTransmission transmission)
+            {
+                this.transmission = transmission;
+            }
+        }
+        static class CarConstructor
+        {
+            public static Car construct(Engine engine, Color color, Transmission transmission)
+            {
+                Car car = new Car(engine, color, transmission);
+                return car;
+            }
+            public static void reconstruct(Car car)
+            {
+                Engine newEngine = new Engine(100500);
+                car.changeEngine(newEngine);
+            }
+        }
+        class Car
+        {
+            Engine engine;
+            Color color;
+            Transmission transmission;
+            public void changeEngine(Engine engine)
+            {
+                this.engine = engine;
+            }
+            public Car(Engine engine, Color color, Transmission transmission)
+            {
+                this.engine = engine;
+                this.color = color;
+                this.transmission = transmission;
+            }
+            public void show()
+            {
+                Console.WriteLine("----------------");
+                Console.WriteLine("Engine power: " + engine.power);
+                Console.WriteLine("Color: " + color.color);
+                Console.WriteLine("Transmission: " + transmission.transmission);
+                Console.WriteLine("----------------");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Engine engine = new Engine(200);
+            Color color = new Color(CarColor.blue);
+            Transmission transmission = new Transmission(CarTransmission.auto);
+            Car myCar = CarConstructor.construct(engine, color, transmission);
+            myCar.show();
+            CarConstructor.reconstruct(myCar);
+            myCar.show();
+
+
+            Console.ReadLine();
+        }
+    }
+}
