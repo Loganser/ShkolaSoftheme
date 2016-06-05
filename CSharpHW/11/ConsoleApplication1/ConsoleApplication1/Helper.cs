684a8158508974da67ba4654d19c004a10167654
Fedorov Sergii <crustydota@gmail.com>
2016-06-05 22:53:07 +0300

    Task 11

---
+++ CSharpHW/11/ConsoleApplication1/ConsoleApplication1/Helper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class Helper
+    {
+        public static void PrintAll(this Program.Printer pr, params string[] s)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("--------------------------------");
+            foreach (string s_i in s)
+                Console.WriteLine(s_i);
+            Console.WriteLine("--------------------------------");
+            Console.ResetColor();
+        }
+
+        public static void PrintAll(this Program.ColourPrinter pr, ConsoleColor color, params string[] s)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine("--------------------------------");
+            foreach (string s_i in s)
+                Console.WriteLine(s_i);
+            Console.WriteLine("--------------------------------");
+            Console.ResetColor();
+        }
+
+        public static void PrintAll(this Program.PhotoPrinter pr, params string[] s)
+        {
+            if (s.Length % 2 == 1) Console.WriteLine("Error");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("--------------------------------");
+            for (int i = 0; i < s.Length; i+=2)
+            {
+                Console.WriteLine("Photo is ready: ");
+                Console.WriteLine(s[i]+" "+s[i+1]);
+            }
+            Console.WriteLine("--------------------------------");
+            Console.ResetColor();
+        }
+
+    }
+}
---
+++ CSharpHW/11/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public class Printer
+        {
+            public virtual void Print(string s)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Base class message: "+s);
+            }
+        }
+        public class ColourPrinter : Printer
+        {
+            public override void Print(string s)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("ColourPrinter class message: " + s);
+            }
+            public void Print(string s, ConsoleColor color)
+            {
+                Console.ForegroundColor = color;
+                Console.WriteLine("ColourPrinter class custom message: " + s);
+            }
+
+        }
+
+        public class PhotoPrinter : Printer
+        {
+            public override void Print(string s)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("PhotoPrinter class message: " + s);
+            }
+            public void Print(string photo, string path)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("ColourPrinter class custom message: " + "photo is ready");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Printer pr1 = new Printer();
+            pr1.Print("Hi");
+            pr1 = new ColourPrinter();
+            pr1.Print("Hi");
+            ColourPrinter pr2 = new ColourPrinter();
+            pr2.Print("Hi", ConsoleColor.Cyan);
+
+            pr1 = new PhotoPrinter();
+            pr1.Print("Hi");
+            PhotoPrinter pr3 = new PhotoPrinter();
+            pr3.Print("myPhoto.jpg", "C\\:");
+
+
+            pr2.PrintAll(ConsoleColor.Blue, "t1", "t2");
+
+            pr1 = new Printer();
+            pr1.PrintAll("t1", "t2");
+
+            pr3.PrintAll("t1", "t2");
+
+            Console.ReadLine();
+        }
+    }
+}
