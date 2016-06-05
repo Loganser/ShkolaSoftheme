ebcde8c7cf93ed61ef90f03264f8678f8e912db2
Fedorov Sergii <crustydota@gmail.com>
2016-06-05 15:27:46 +0300

    Task 9

---
+++ CSharpHW/9/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        [Flags]
+        enum FileAccessEnum : short
+        {
+            Read = 1,
+            Write = 2
+        }
+        struct FileHandler
+        {
+            int FileSize;
+            string FileName;
+            string FilePath;
+            FileAccessEnum FileAccess;
+
+            public FileHandler(string FileName, string FilePath, FileAccessEnum FileAccess)
+            {
+                this.FileName = FileName;
+                this.FileSize = 0;
+                this.FilePath = FilePath;
+                this.FileAccess = FileAccess;
+            }
+
+            public void OpenForRead()
+            {
+                string path = FilePath + "\\" + FileName;
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, (System.IO.FileAccess)FileAccessEnum.Read))
+                {
+                    
+                }
+
+            }
+
+            public void OpenForWrite()
+            {
+                string path = FilePath + "\\" + FileName;
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, (System.IO.FileAccess)FileAccessEnum.Write))
+                {
+
+                }
+
+            }
+
+            public void OpenFile()
+            {
+                string path = FilePath + "\\" + FileName;
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate, (System.IO.FileAccess)FileAccessEnum.Read | (System.IO.FileAccess)FileAccessEnum.Write)) // 1 | 2 = 3
+                {
+
+                }
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string fullPath = Directory.GetCurrentDirectory()+ "\\file.txt";
+            FileHandler file = new FileHandler("file.txt", Directory.GetCurrentDirectory(), FileAccessEnum.Read| FileAccessEnum.Write);
+            file.OpenForRead();
+            file.OpenForWrite();
+            file.OpenFile();
+
+            Console.ReadLine();
+        }
+    }
+}
