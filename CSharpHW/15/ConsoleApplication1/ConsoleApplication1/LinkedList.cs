e92662c6cc8523324dd2b0aed6b2aa6616914346
Loganser <fedorovsergiy94@gmail.com>
2016-06-13 11:09:00 +0300

    Task 15

---
+++ CSharpHW/15/ConsoleApplication1/ConsoleApplication1/LinkedList.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class LinkedList <T>
+    {
+        public class node
+        {
+            public T data;
+            public node next;
+            public node prev;
+            public node(T data)
+            {
+                this.data = data;
+                next = null;
+                prev = null;
+            }
+            public node()
+            {
+                next = null;
+                prev = null;
+            }
+        }
+        public node head { get; set; }
+        node current;
+
+        public void Add(T d)
+        {
+            if (head == null)
+            {
+                head = new node(d);
+                current = head;
+                return;
+            }
+            node tmp = current;
+            current.next = new node(d);
+            current = current.next;
+            current.prev = tmp;
+        }
+
+        public int Count()
+        {
+            int res = 0;
+            node tmp = head;
+            while (tmp != null)
+            {
+                tmp = tmp.next;
+                ++res;
+            }
+            return res;
+        }
+
+        public T[] ToArray()
+        {
+            T[] res = new T[Count()];
+            int iter = 0;
+            node tmp = head;
+            while (tmp != null)
+            {
+                res[iter++] = tmp.data;
+                tmp = tmp.next;
+            }
+            return res;
+        }
+
+        public bool Find(T d)
+        {
+            node tmp = head;
+            while (tmp != null && !tmp.data.Equals(d)) tmp = tmp.next;
+            if (tmp != null) return true;
+            return false;
+        }
+
+        public void Delete(node n)
+        {
+            node tmp = head;
+            while (tmp != null && tmp != n) tmp = tmp.next;
+            if (tmp == head)
+            {
+                head = head.next;
+                return;
+            }
+            tmp.prev.next = tmp.next;
+        }
+
+    }
+}
---
+++ CSharpHW/15/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        class MyClass
+        {
+            public int data;
+            public MyClass()
+            {
+                data = 0;
+            }
+            public MyClass(int d)
+            {
+                data = d;
+            }
+        }
+        static void Main(string[] args)
+        {
+            LinkedList<MyClass> L = new LinkedList<MyClass>();
+            MyClass mc = new MyClass(1);
+            L.Add(mc);
+            mc = new MyClass(2);
+            L.Add(mc);
+            mc = new MyClass(3);
+            L.Add(mc);
+            Console.WriteLine(L.Count());
+            Console.WriteLine(L.Find(mc));
+            mc = new MyClass(4);
+            Console.WriteLine(L.Find(mc));
+            MyClass[] mc_array = L.ToArray();
+            for (int i = 0; i < mc_array.Count(); ++i)
+                Console.WriteLine(mc_array[i].data);
+
+            Console.WriteLine();
+            L.Delete(L.head);
+
+            mc_array = L.ToArray();
+            for (int i = 0; i < mc_array.Count(); ++i)
+                Console.WriteLine(mc_array[i].data);
+
+            Console.ReadLine();
+        }
+    }
+}
